7afb61bdd13854a2de38a2b7638812fb99e7588e
Tran Manh Khiem <khiemmanhtran@gmail.com>
2026-04-03 02:14:13 +0700

    add demo

--- src/main/java/com/example/demo_sqli/repository/Repository.java
+++ src/main/java/com/example/demo_sqli/repository/Repository.java
@@ -1,24 +1,69 @@
 package com.example.demo_sqli.repository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 @org.springframework.stereotype.Repository
-
-
 public class Repository {
+
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
-    public boolean login(String username, String password){
-        String sql = "SELECT count(*) FROM users WHERE username = '" + username + "' AND password = '" + password + "'";
-        System.out.println("--------------------------------------------------");
-        System.out.println("[SQL EXECUTING - LOGIN]: " + sql);
-        System.out.println("--------------------------------------------------");
+    @Value("${app.security.mode}")
+    private String mode;
 
+    public boolean login(String username, String password) {
+
+        System.out.println("====== MODE: " + mode + " ======");
+
+        // 8081 cho docker, 8080 run thuong
+        // dung: http://localhost:8081/login?username=admin&password=admin123
         try {
-            int count = jdbcTemplate.queryForObject(sql, Integer.class);
-            return count > 0;
+            // ================= VULN =================
+            // http://localhost:8081/login?username=admin' OR '1'='1&password=abc
+            if (mode.equals("VULN")) {
+                String sql = "SELECT count(*) FROM users WHERE username = '"
+                        + username + "' AND password = '" + password + "'";
+
+                System.out.println("[SQL VULN]: " + sql);
+
+                int count = jdbcTemplate.queryForObject(sql, Integer.class);
+                return count > 0;
+            }
+
+            // ================= FILTER =================
+            // http://localhost:8081/login?username=admin%5C&password=%20OR%201=1%20--%20
+            // "http://localhost:8081/login?username=admin\&password= OR 1=1 -- "
+            else if (mode.equals("FILTER")) {
+                username = username.replace("'", "");
+                password = password.replace("'", "");
+
+                String sql = "SELECT count(*) FROM users WHERE username = '"
+                        + username + "' AND password = '" + password + "'";
+
+                System.out.println("[SQL FILTER]: " + sql);
+
+                int count = jdbcTemplate.queryForObject(sql, Integer.class);
+                return count > 0;
+            }
+
+            // ================= SECURE =================
+            else {
+                String sql = "SELECT count(*) FROM users WHERE username = ? AND password = ?";
+
+                System.out.println("[SQL SECURE]: " + sql);
+
+                Integer count = jdbcTemplate.queryForObject(
+                        sql,
+                        Integer.class,
+                        username,
+                        password
+                );
+
+                return count != null && count > 0;
+            }
+
         } catch (Exception e) {
             e.printStackTrace();
             return false;
